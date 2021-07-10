7e5bb56eb2c8ec3c70f3e80bf18e2c1a8330ff72
Mariusz Perzynski <m.perzyk@interia.pl>
2021-07-10 15:02:13 +0200

    basic server settings

--- socket.py
+++ socket.py
@@ -7,4 +7,11 @@ PORT = 33000
 
 server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
 # inet = ipv4 / inet6 = ipv6
+
 server_socket.bind((HOST,PORT))
+server_socket.listen(2)
+# 2/3 listen ,,channel,,?
+
+while True:  
+    client_socket, address = server_socket.accept()
+    print(f"Accept Connect from {address[0]}:{address[1]}")
