2ea642c62ba00963fd0ff09968fa955728dc438a
Mariusz Perzynski <m.perzyk@interia.pl>
2021-07-10 14:54:41 +0200

    upgrade basic data with ipv4/6

--- socket.py
+++ socket.py
@@ -1,1 +1,10 @@
-import socket as s
+# FROM SERVER
+
+import socket as sc
+
+HOST = '128.125.1.13'
+PORT = 33000
+
+server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+# inet = ipv4 / inet6 = ipv6
+server_socket.bind((HOST,PORT))
