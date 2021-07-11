6ad708330c23954266a3e1a937eca29caadb3a08
Mariusz Perzynski <m.perzyk@interia.pl>
2021-07-11 14:48:45 +0200

    update branch

---
+++ client_one_socket.py
@@ -0,0 +1,9 @@
+# FROM CLIENT v1
+
+import socket as sc
+
+HOST = '128.125.1.13'
+PORT = 33000
+
+server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+# inet = ipv4 / inet6 = ipv6
--- client_two_socket.py
+++ client_two_socket.py
@@ -1,9 +1,9 @@
-# FROM CLIENT
+# FROM CLIENT v2
 
 import socket as sc
 
-# HOST = '128.125.1.13'
-# PORT = 33000
+HOST = '128.125.1.14'
+PORT = 33001
 
 server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
 # inet = ipv4 / inet6 = ipv6
