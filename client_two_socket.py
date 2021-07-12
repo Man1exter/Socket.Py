9563fe392f4b650d02a1c4e365ffc265e20d672f
Mariusz Perzynski <m.perzyk@interia.pl>
2021-07-12 11:30:37 +0200

    changes with variables on twice users to cn

--- client_two_socket.py
+++ client_two_socket.py
@@ -5,5 +5,6 @@ import socket as sc
 HOST = '128.125.1.14'
 PORT = 33001
 
-server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+client_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
 # inet = ipv4 / inet6 = ipv6
+client_socket.connect((HOST,PORT))
